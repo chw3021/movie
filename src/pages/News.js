880aed7b5600acd391f7d51e9ad3e7cc70f5200c
irenek-gyo <irenekang95@gmail.com>
2025-01-22 14:35:19 +0900

    250122

---
+++ src/pages/News.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+
+const News =()=>{
+    return(
+        <div>
+            <Header />
+            <h1>Movie News</h1>
+            
+        </div>
+    )
+
+}
+
+export default News;
