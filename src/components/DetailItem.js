71ed5ebb38b68542ac67a247a96c19e0c09234fa
chw3021 <ckgusdnr1205@gmail.com>
2025-01-22 17:07:35 +0900

    250122

--- src/components/DetailItem.js
+++ src/components/DetailItem.js
@@ -1,26 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './DetailItem.css';
 
 const DetailItem = ({ title, genre, img, link }) => {
-  const [content, setContent] = useState('');
+  
+  
+  const youtubeEmbedUrl = ``
 
-  useEffect(() => {
-    const fetchContent = async () => {
-      try {
-        const response = await fetch(link);
-        const text = await response.text();
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(text, 'text/html');
-        const bodyContent = doc.querySelector('#bodyContent');
-        setContent(bodyContent ? bodyContent.innerHTML : 'Content not available');
-      } catch (error) {
-        console.error('Error fetching content:', error);
-        setContent('Error fetching content');
-      }
-    };
-
-    fetchContent();
-  }, [link]);
   return (
     <div className="detail-item">
       <h1>{title}</h1>
@@ -28,10 +13,16 @@ const DetailItem = ({ title, genre, img, link }) => {
       {img ? (
         <img src={img} alt={`${title} poster`} />
       ) : (
-        <div>이미지가 유효하지 않습니다.</div>
+        <div>No image available</div>
       )}
-      <div className="content" dangerouslySetInnerHTML={{ __html: content }} />
-      <a href={link} target="_blank" rel="noopener noreferrer">More Info</a>
+      <div className="iframe-container">
+        <iframe
+          src={youtubeEmbedUrl}
+          title={title}
+          className="scaled-iframe"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 };
