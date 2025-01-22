58ff49f86f390ca77f18fd1e358002dce1860d41
excel8057 <excel8057@naver.com>
2025-01-22 11:14:58 +0900

    250122

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -9,7 +9,7 @@ const MovieItem = ({ id, title, genre, img }) => {
   };
 
   return (
-    <div className="movie-item" onClick={handleClick}>
+    <div className="MovieItem" onClick={handleClick}>
       <img src={img} alt={title} />
       <h3>{title}</h3>
       <p>{genre}</p>
