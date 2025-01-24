c4a076b258b5d1af8417547be1e22bb794bbf999
excel8057 <excel8057@naver.com>
2025-01-24 09:10:37 +0900

    250124

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,18 @@
-import './NewsItem.css';
+import React from "react";
+import "./NewsItem.css";
 
-
-const NewsItem=()=>{
-    return(
-        <div>
-            <h3>NewsItem 게시글</h3>
-        </div>
-    )
-
-}
+const NewsItem = ({ id, title, summary, date, externalUrl }) => {
+  return (
+    <li className="news-item">
+      <h2>
+        <a href={externalUrl} target="_blank" rel="noopener noreferrer">
+          {title}
+        </a>
+      </h2>
+      <p className="news-summary">{summary}</p>
+      <p className="news-date">{date}</p>
+    </li>
+  );
+};
 
 export default NewsItem;
